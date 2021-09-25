a50e7a86e00cde329427bdabcc95983e7f01f6cd
finleywood <finleyjohnwood@gmail.com>
2021-09-25 15:25:29 +0100

    updated routes/setup.go

--- routes/setup.go
+++ routes/setup.go
@@ -1,11 +1,18 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 var app *fiber.App
 
 func Run() {
 	app = fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	setupRoutes()
 
